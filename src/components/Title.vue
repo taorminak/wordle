8673c561de70ca54f37e92c1bfeebf79ad092ca8
Anna Ostanina <a.ostanina@sportradar.com>
2024-06-14 10:46:36 +0200

    init: start the wordle project

---
+++ src/components/Title.vue
@@ -0,0 +1,26 @@
+<template >
+    <div class="header_title">
+        <div>Wordle</div>
+    </div>
+</template>
+
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+.header_title {
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 24px;
+    font-style: italic;
+    font-family: Verdana, Geneva, Tahoma, sans-serif;
+    font-weight:900;
+}
+    
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<script setup>
+
+</script>
+
+<template>
+  <main>
+ <div>This is home page</div>
+  </main>
+</template>
